6263179b94ecf4dd82972928a14d6c64df86c2ab
Rafael Braga <rafaelbra149@gmail.com>
2025-09-01 13:57:17 +0100

    Fixing signIn to Github bug

--- auth.ts
+++ auth.ts
@@ -1,10 +1,17 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+const prisma = new PrismaClient()
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-});
-
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+})
