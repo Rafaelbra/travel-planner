458042f204e9856d147be3b5b7248f4ccd37a7de
Rafael Braga <rafaelbra149@gmail.com>
2025-09-03 10:31:34 +0100

    Introducing upload image button using uploadthing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{
+      hostname: "8isat4mq3c.ufs.sh",
+    },
+  ],
+  },
 };
 
 export default nextConfig;
