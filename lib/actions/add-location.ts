78b202639aa7a6577068fab2a04deba32a440b4e
Rafael Braga <rafaelbra149@gmail.com>
2025-09-11 18:30:19 +0300

    *bug: geometry location not recognised from data API

--- lib/actions/add-location.ts
+++ lib/actions/add-location.ts
@@ -1,6 +1,8 @@
 "use server"
 
 import { auth } from "@/auth"
+import { prisma } from "../prisma";
+import { redirect } from "next/navigation";
 
 async function geocodeAddress(address:string) {
     const apiKey = process.env.GOOGLE_MAPS_API_KEY!;
@@ -9,6 +11,10 @@ async function geocodeAddress(address:string) {
          address
         )}&key=${apiKey}`
     );
+
+    const data = await response.json()
+    const { lat, lng } = data.results[0].geometry.location;
+    return { lat, lng };
 }
 
 export async function addLocation(formData:FormData, tripId: string) {
@@ -23,6 +29,21 @@ export async function addLocation(formData:FormData, tripId: string) {
         throw new Error("Missing address");
     }
 
+    const {lat, lng} = await geocodeAddress(address);
 
-    
+    const count = await prisma.location.count({
+        where: { tripId },
+    });
+
+    await prisma.location.create({
+        data: {
+            locationTitle: address, 
+            lat,
+            lng,
+            tripId,
+            order: count
+        },
+    });
+
+    redirect(`/trips/${tripId}`);
 }
