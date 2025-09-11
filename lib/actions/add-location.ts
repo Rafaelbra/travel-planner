c3e2b5165d5b2a15c9d9e0d2c74bfd4150bd63bc
Rafael Braga <rafaelbra149@gmail.com>
2025-09-11 17:47:50 +0300

    Fetching longitude and latitude  from google maps API

--- lib/actions/add-location.ts
+++ lib/actions/add-location.ts
@@ -2,11 +2,25 @@
 
 import { auth } from "@/auth"
 
+async function geocodeAddress(address:string) {
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY!;
+    const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+         address
+        )}&key=${apiKey}`
+    );
+}
+
 export async function addLocation(formData:FormData, tripId: string) {
 
     const session = await auth()
     if (!session) {
         throw new Error("Not authenticated")
+    }
+
+    const address = formData.get("address")?.toString()
+    if (!address) {
+        throw new Error("Missing address");
     }
 
 
