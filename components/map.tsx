d8f3dafbdb5edbd7a9054d2502d09a4b83f86c63
Rafael Braga <rafaelbra149@gmail.com>
2025-09-12 15:55:45 +0300

    Adding maps banner from GoogleMaps API

--- components/map.tsx
+++ components/map.tsx
@@ -1,10 +1,28 @@
+"use client"
+
+import { Location } from "@prisma/client";
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
 
 interface MapProps {
     itinenaries: Location[];
-}
+};
 
 export default function Map({ itinenaries } : MapProps) {
+    const  { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+    });
+
+    if (loadError) {
+        return <div> Error loading maps</div>
+    };
+
+    if (!isLoaded) {
+        return <div> Loading maps...</div>
+    };
+
     return (
-        <h1>Hi</h1>
+        <GoogleMap mapContainerStyle={{width: "100%", height: "100%"}}>
+
+        </GoogleMap>
     )
 };
